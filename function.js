a3769481bb1524ee248bf4893bd76d81c5ff9972
lmichelletech <lmichelle@techlaunch.io>
2018-03-28 23:00:55 -0400

    first commit

---
+++ function.js
@@ -0,0 +1,19 @@
+var a = 3;
+function  getUser(id,name,email){
+
+    var id = id
+    var name = name
+    var email = email 
+    console.log(`this is id id  ${id}...`) 
+}
+
+function sendBack(x){
+    return x * 3;
+}
+
+//getUser("AB1234", "Lazara", "lmichelle@techlaunch")
+
+var b = sendBack(a);
+
+console.log(b);
+
